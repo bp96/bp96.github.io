ae27b5a7e3625708d7886a19752b1d6fb77959e9
bp96 <brijeshpatel96@gmail.com>
2020-12-27 15:42:27 +0000

    updating to add web dev code

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let websitesElement = document.getElementById("websites-section");
+let dataScienceElement = document.getElementById("data-science-section");
+let dataScienceBtn = document.getElementById("data-science-btn");
+let websitesBtn = document.getElementById("websites-btn");
+
+function hideAndShowWebsites() {
+  websitesElement.style.display = "block";
+  dataScienceElement.style.display = "none";
+
+  dataScienceBtn.classList.remove("active");
+  websitesBtn.classList.add("active");
+
+}
+
+function hideAndShowDataScience() {
+  dataScienceElement.style.display = "block";
+  websitesElement.style.display = "none";
+  dataScienceBtn.classList.add("active");
+  websitesBtn.classList.remove("active");
+
+}
+
+if (window.screen.width > 780) {
+  document.write( 
+    '<script type="text/javascript" src="js/vanilla-tilt.js"></script>' 
+
+  )
+}
