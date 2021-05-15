10a392635d3d206c0309fbeec760811b28f21c20
bp96 <brijeshpatel96@gmail.com>
2021-05-15 11:23:20 +0100

    adding hash elements to URL on page load

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let dataScienceElement = document.getElementById("data-science-section");
 let dataScienceBtn = document.getElementById("data-science-btn");
 let websitesBtn = document.getElementById("websites-btn");
 
+// Functions
 function hideAndShowWebsites() {
   websitesElement.style.display = "block";
   dataScienceElement.style.display = "none";
@@ -11,6 +12,7 @@ function hideAndShowWebsites() {
   dataScienceBtn.classList.remove("active");
   websitesBtn.classList.add("active");
   window.scrollBy(0, 1); // scrolls by 1 px to fix bug where projects do not appear until you scroll when switching between websites/data science
+  window.location.hash='websites'; // modifies URL by adding #websites
 
 }
 
@@ -20,5 +22,19 @@ function hideAndShowDataScience() {
   dataScienceBtn.classList.add("active");
   websitesBtn.classList.remove("active");
   window.scrollBy(0, 1); // scrolls by 1 px to fix bug where projects do not appear until you scroll when switching between websites/data science
+  window.location.hash='datascience'; // modifies URL by adding #websites
 
 }
+
+function checkHashOnPageLoad(){
+  var hashValue = window.location.hash.substr(1);
+
+  if(hashValue=="websites") {
+    hideAndShowWebsites();
+  } else if (hashValue=="datascience") {
+    hideAndShowDataScience();
+  } 
+}
+
+// Run when script is loaded
+checkHashOnPageLoad();
